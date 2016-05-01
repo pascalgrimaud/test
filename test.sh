88c73bb7bde9e55c53b1f8472578e79ce60a89a4
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-05-01 13:15:08 +0200

    test19

--- test.sh
+++ test.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
+set -ev
 
-waitForProtractor() {
-    echo "waitForProtractor"
+PROTRACTOR=1
+
+wlaunchProtractor() {
     retryCount=0
-    maxRetry=5
+    maxRetry=10
     httpUrl="http://localhost:8080"
 
     rep=$(curl -v "$httpUrl")
     status=$?
     while [ "$status" -ne 0 ] && [ "$retryCount" -ne "$maxRetry" ]; do
-      retryCount=$((retryCount+1))
-      echo "[$(date)] application not reachable yet. Sleep and retry. retryCount =" $retryCount "/" $maxRetry
-      sleep 5
-
-      rep=$(curl -v "$httpUrl")
-      status=$?
+        echo "[$(date)] Application not reachable yet. Sleep and retry - retryCount =" $retryCount "/" $maxRetry
+        retryCount=$((retryCount+1))
+        sleep 10
+        rep=$(curl -v "$httpUrl")
+        status=$?
     done
 
-    if [ $status -ne 0 ]; then
+    if [ "$status" -ne 0 ]; then
       echo "[$(date)] Not connected after" $retryCount " retries. Abort protractor tests."
-      exit 1
+      exit 0
     fi
 
-    echo "[$(date)] Connected to application"
+    echo "[$(date)] Connected to application. Start protractor tests."
+    gulp itest --no-notification
 }
 
-waitForProtractor
+if [ "$PROTRACTOR" == 1 ]; then
+  launchProtractor
+fi
