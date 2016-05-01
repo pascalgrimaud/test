8b07c86b76bc07ea8a378262f16c5767b3c3673b
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-05-01 10:48:51 +0200

    test10

--- test.sh
+++ test.sh
@@ -4,18 +4,12 @@ waitForProtractor() {
     echo "waitForProtractor"
     retryCount=0
     maxRetry=5
-    while [ "$retryCount" -ne "$maxRetry" ]; do
-        curl http://localhost:8080 &>/dev/null && break
-        echo "Application not reachable yet. Sleep and retry - retryCount =" $retryCount
-        sleep 5
-        ((retryCount+=1))
-    done
-
-    if [ "$retryCount" -eq "$maxRetry" ]; then
+    echo "Before while"
+    curl --retry 5 --retry-delay 5 http://localhost:8080 &>/dev/null
+    if [ $? -ne 0 ]; then
       echo "not connected after" $retryCount " retries. Abort protractor tests."
-      exit 0
+      exit 1
     fi
-
     echo "Connected to application"
 }
 
