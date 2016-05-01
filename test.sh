b52defa3e05671f8d1356787a5629666c0c6cde6
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-05-01 10:39:32 +0200

    test8

--- test.sh
+++ test.sh
@@ -6,14 +6,14 @@ function waitForProtractor() {
     maxRetry=5
     echo "1st curl"
     curl http://localhost:8080 &>/dev/null
-    while [ $? -ne 0 ] && [ "$retryCount" -ne "$maxRetry" ]; do
+    while [ "$retryCount" -ne "$maxRetry" ]; do
+        curl http://localhost:8080 &>/dev/null && break
         echo "Application not reachable yet. Sleep and retry - retryCount =" $retryCount
         sleep 5
         ((retryCount+=1))
-        curl http://localhost:8080 &>/dev/null
     done
 
-    if [ $? -ne 0 ]; then
+    if [ "$retryCount" -eq "$maxRetry" ]; then
       echo "not connected after" $retryCount " retries. Abort protractor tests."
       exit 0
     fi
