554479a5f102d3375df6be063685685f927ad6f7
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-05-01 11:27:26 +0200

    test11

--- test.sh
+++ test.sh
@@ -4,13 +4,15 @@ waitForProtractor() {
     echo "waitForProtractor"
     retryCount=0
     maxRetry=5
-    echo "Before while"
+    echo "[$(date)]: Before while"
     curl --retry 5 --retry-delay 5 http://localhost:8080 &>/dev/null
     if [ $? -ne 0 ]; then
       echo "not connected after" $retryCount " retries. Abort protractor tests."
       exit 1
     fi
+
     echo "Connected to application"
+    echo "[$(date)]: End"
 }
 
 waitForProtractor
