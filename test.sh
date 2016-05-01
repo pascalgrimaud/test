453b5326b07588601d31bf42c2068dc5cc04a5e4
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-05-01 11:35:13 +0200

    test12

--- test.sh
+++ test.sh
@@ -5,8 +5,12 @@ waitForProtractor() {
     retryCount=0
     maxRetry=5
     echo "[$(date)]: Before while"
-    curl --retry 5 --retry-delay 5 http://localhost:8080 &>/dev/null
-    if [ $? -ne 0 ]; then
+    httpUrl="http://localhost:8080"
+    rep=$(curl -v "$httpUrl")
+    status=$?
+    echo "$rep"
+    echo "$status"
+    if [ $status -ne 0 ]; then
       echo "not connected after" $retryCount " retries. Abort protractor tests."
       exit 1
     fi
