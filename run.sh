6e05628991ca20c86077066cf0c55131bb9ed2e3
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-05-10 07:21:59 +0200

    test

--- run.sh
+++ run.sh
@@ -3,29 +3,29 @@
 docker run \
   -d --name app-mysql \
   -v "$TRAVIS_BUILD_DIR"/samples/app-mysql:/home/jhipster/volume/app:ro \
+  -t tester
   # -v /home/pgrimaud/.m2:/home/jhipster/volume/.m2:ro \
   # -v "$TRAVIS_BUILD_DIR"/node_modules:/home/jhipster/volume/node_modules:ro \
   # -v "$TRAVIS_BUILD_DIR"/generator-jhipster:/home/jhipster/volume/generator-jhipster:ro \
   # --memory="768m" \
-  -t tester
 
 docker run \
   -d --name app-cassandra \
   -v "$TRAVIS_BUILD_DIR"/samples/app-cassandra:/home/jhipster/volume/app:ro \
+  -t tester
   # -v /home/pgrimaud/.m2:/home/jhipster/volume/.m2:ro \
   # -v "$TRAVIS_BUILD_DIR"/node_modules:/home/jhipster/volume/node_modules:ro \
   # -v "$TRAVIS_BUILD_DIR"/generator-jhipster:/home/jhipster/volume/generator-jhipster:ro \
   # --memory="768m" \
-  -t tester
 
 docker run \
   -d --name app-mongodb \
   -v "$TRAVIS_BUILD_DIR"/samples/app-mongodb:/home/jhipster/volume/app:ro \
+  -t tester
   # -v /home/pgrimaud/.m2:/home/jhipster/volume/.m2:ro \
   # -v "$TRAVIS_BUILD_DIR"/node_modules:/home/jhipster/volume/node_modules:ro \
   # -v "$TRAVIS_BUILD_DIR"/generator-jhipster:/home/jhipster/volume/generator-jhipster:ro \
   # --memory="768m" \
-  -t tester
 
 # docker run \
 #   -d --name app-oauth2 \
